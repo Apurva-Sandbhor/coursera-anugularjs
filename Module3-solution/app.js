9216d1ef2a069b1d80b2df9e427c7f61e563c9e5
Apurva Sandbhor <apurvasandbhor@gmail.com>
2017-11-14 01:21:34 -0500

    code upload

--- Module3-solution/app.js
+++ Module3-solution/app.js
@@ -10,16 +10,66 @@ angular.module("ShoppingListCheckOff", [])
 
 NarrowItDownController.$inejct = ['MenuSearchService']
 function NarrowItDownController(MenuSearchService){
+	var ctrl = this;
+	ctrl.searchTerm = "";
+	ctrl.narrowIt = function(){
+		if(ctrl.searchTerm === ""){
+			ctrl.items = [];
+			return;
+		}
+		var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+		promise.then(function(response){
+			ctrl.items = response;
+		})
+		.catch(function(err){
+			console.log("Error", err);
+		})
 
+		ctrl.removeItem = function(index){
+			ctrl.items.splice(index, 1);
+		}
+	}
 }
 
 MenuSearchService.$inject = ['$http']
 function MenuSearchService($http){
+	var service = this;
 
+	service.getMatchedMenuItems = function(searchTerm){
+		return $http({
+			url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+		}).then(function(result){
+			var items = result.data.menu_items;
+			var foundItems = [];
+			for(var i = 0; i < items.length; i++){
+				if(items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0){
+					foundItems.push(items[i]);
+				}
+			}
+			return foundItems;
+		});
+	}
 }
 
 function FoundItemsDirective(){
-	
+	var ddo = {
+		templateUrl: 'foundItems.html',
+		scope: {
+			found: '<',
+			onRemove: '&'
+		},
+		controller: FoundItemsDirectiveController,
+		controllerAs: 'list',
+		bindToController: true
+	};
+	return ddo;
+}
+
+function FoundItemsDirectiveController(){
+	var list = this;
+	list.isEmpty = function(){
+		return list.found != undefined && list.found.length === 0;
+	}
 }
 
 })();
